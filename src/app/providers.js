78e00daeeaaaeb98d5ed0f7aa8f594c8545a1ef4
MichaelMoyles <moylesm15@gmail.com>
2025-01-17 14:29:00 +0000

    feat(tutorial): complete step 1

---
+++ src/app/providers.js
@@ -0,0 +1,18 @@
+'use client';
+
+import TutorialHeader from '@/components/TutorialHeader/TutorialHeader';
+import { Content, Theme } from '@carbon/react';
+
+export function Providers({ children }) {
+  return (
+    <div>
+      <div>
+        <Theme theme="g100">
+          <TutorialHeader />
+        </Theme>
+        <Content>{children}</Content>
+      </div>
+      <Content>{children}</Content>
+    </div>
+  );
+}
